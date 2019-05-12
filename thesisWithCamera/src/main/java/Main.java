959edc6d553bdda7ab779161c363bb55d7ce8020
Mikhail <mishav147@gmail.com>
2019-05-12 16:10:49 +0200

    Add files via upload

---
+++ thesisWithCamera/src/main/java/Main.java
@@ -0,0 +1,62 @@
+
+import com.github.sarxos.webcam.Webcam;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author MVPIMP
+ */
+public class Main {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        IamOptions options = new IamOptions.Builder()
+                .apiKey("sP83C9a-mEePlb5-4banZN1EYLSbxavGnpYmOLYGl-v7")
+                .build();
+        
+        VisualRecognition service = new VisualRecognition("2018-03-19", options);
+        
+        Scanner command = new Scanner(System.in);
+        
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+        
+        
+        System.out.println("Type 'takepic' to take a picture and analyse it.");
+        while (true) {
+            
+            String userInput = command.next();
+            if (userInput.equalsIgnoreCase("takepic")) {
+                ImageIO.write(webcam.getImage(), "PNG", new File("hello-world.png"));
+
+                //InputStream imagesStream = new FileInputStream("C:\\Users\\MVPIMP\\Pictures\\Watson test\\Carrots\\images.jpg");
+                InputStream imagesStream = new FileInputStream("hello-world.png");
+                ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+                        .imagesFile(imagesStream)
+                        .imagesFilename("download.jpg")
+                        .threshold((float) 0.6)
+                        .classifierIds(Arrays.asList("DefaultCustomModel_1716876290"))
+                        .build();
+                
+                ClassifiedImages result = service.classify(classifyOptions).execute();
+                System.out.println(result);
+                
+            }
+        }
+    }
+}

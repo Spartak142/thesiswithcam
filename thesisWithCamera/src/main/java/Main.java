a8debf1ba69f4793a73a839074d21fa6fb12ff82
Spartak142 <mishav147@gmail.com>
2019-05-12 23:41:26 +0200

    Fixed picture resolution 1024 x 768

--- thesisWithCamera/src/main/java/Main.java
+++ thesisWithCamera/src/main/java/Main.java
@@ -1,9 +1,13 @@
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+
 import com.ibm.watson.developer_cloud.service.security.IamOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -23,26 +27,28 @@ import javax.imageio.ImageIO;
  * @author MVPIMP
  */
 public class Main {
-    
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
         IamOptions options = new IamOptions.Builder()
                 .apiKey("sP83C9a-mEePlb5-4banZN1EYLSbxavGnpYmOLYGl-v7")
                 .build();
-        
+
         VisualRecognition service = new VisualRecognition("2018-03-19", options);
-        
+
         Scanner command = new Scanner(System.in);
-        
+
         Webcam webcam = Webcam.getDefault();
+        webcam.setViewSize(new Dimension(1024, 768));
+
         webcam.open();
-        
-       System.out.println("Tjena bror!");
+
         System.out.println("Type 'takepic' to take a picture and analyse it.");
         while (true) {
-            
+
             String userInput = command.next();
             if (userInput.equalsIgnoreCase("takepic")) {
-                ImageIO.write(webcam.getImage(), "PNG", new File("hello-world.png"));
+                BufferedImage image = webcam.getImage();
+                ImageIO.write(image, "PNG", new File("hello-world.png"));
 
                 //InputStream imagesStream = new FileInputStream("C:\\Users\\MVPIMP\\Pictures\\Watson test\\Carrots\\images.jpg");
                 InputStream imagesStream = new FileInputStream("hello-world.png");
@@ -52,10 +58,10 @@ public class Main {
                         .threshold((float) 0.6)
                         .classifierIds(Arrays.asList("DefaultCustomModel_1716876290"))
                         .build();
-                
+
                 ClassifiedImages result = service.classify(classifyOptions).execute();
                 System.out.println(result);
-                
+
             }
         }
     }
